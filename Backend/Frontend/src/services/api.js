8713202e8bf0baf638b683cab63294f8c531f462
Andres Mallea <andresmallea3@gmail.com>
2026-06-23 06:48:26 -0400

    arreglos para uso de react en el frontend

--- Backend/Frontend/src/services/api.js
+++ Backend/Frontend/src/services/api.js
@@ -1,10 +1,20 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api', 
-  headers: {
-    'Content-Type': 'application/json',
-  },
+  baseURL: 'http://localhost:8080', 
+  withCredentials: true, 
 });
+
+
+export const login = async (username, password) => {
+  const response = await api.post('/api/login/', { username, password });
+  return response.data;
+};
+
+
+export const getArtworks = async () => {
+  const response = await api.get('/api/artworks/');
+  return response.data;
+};
 
 export default api;
