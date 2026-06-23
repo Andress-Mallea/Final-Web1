d6d7688d5fd8ee92f610092d10001b7232d6c2af
Andres Mallea <andresmallea3@gmail.com>
2026-06-23 03:36:47 -0400

    Configuracion modulo catalog-Django

---
+++ Backend/django_app/catalog/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from .models import Category, Tag, Artwork
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    prepopulated_fields = {'slug':('name')}
+@admin.register(Tag)
+class TagAdmin(admin.Model.Admin):
+    list_display = ('name',)
+    search_fields = ('name',)
+@admin.register(Artwork)
+class ArtworlAdmin(admin.ModelAdmin):
+    list_display = ('title', 'artist', 'category', 'views_count', 'created_at')
+    list_filter = ('category', 'created_at')
+    search_fields = ('title', 'artist__username')
+    dilter_horizontal = ('tags'),
---
+++ Backend/django_app/catalog/models.py
@@ -0,0 +1,27 @@
+import uuid
+from django.db import models
+from identity.models import User
+
+class Category(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+    def __str__(self):
+        return self.name
+class Tag(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=50, unique=True)
+    def __str__(self):
+        return self.name
+class Artwork(models.Model):
+    id = models.UUIField(primary_key=True, default=uuid.uuid4, editable=False)
+    artist = models.ForeignKey(User, on_delete=models.CASCADE, related_name='artworks')
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    image_url = models.URLField()
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, related_name='artworks')
+    tags = models.ManyToManyField(Tag, related_name='artworks', blank=True)
+    views_count = models.PositiveIntergerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return f"{self.title} by {self.artist.username}"
