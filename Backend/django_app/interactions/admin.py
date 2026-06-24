5c31199187d0bb760205eab805ac537c32e83f4c
Rodrigo Gamarra <rodrigo007g987@gmail.com>
2026-06-24 13:00:48 -0400

    cambios para base de datos del chat

--- Backend/django_app/interactions/admin.py
+++ Backend/django_app/interactions/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Comment, Favorite
+from .models import Comment, Conversation, Favorite, Message
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
@@ -11,3 +11,13 @@ class CommentAdmin(admin.ModelAdmin):
 class FavoriteAdmin(admin.ModelAdmin):
     list_display = ('user', 'artwork', 'created_at')
     search_fields = ('user__username', 'artwork__title')
+
+@admin.register(Conversation)
+class ConversationAdmin(admin.ModelAdmin):
+    list_display = ('participant_one', 'participant_two', 'updated_at')
+    search_fields = ('participant_one__username', 'participant_two__username')
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('conversation', 'sender', 'created_at')
+    search_fields = ('body', 'sender__username')
